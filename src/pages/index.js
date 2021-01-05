032380982e0dee9bd032e4844a472404477d4774
Will Breiler <will.breiler@gmail.com>
2021-01-05 17:13:44 -0600

    Added 404 page hyperlink

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/404/">Go to 404 pages</Link>
   </div>
 )
 
